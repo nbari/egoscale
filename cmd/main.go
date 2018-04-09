6c5ca99a71e64ca8de13140ea31b3aa0e5e91658
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-04-09 18:16:25 +0200

    cli: do the actual call
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,13 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/user"
+	"path/filepath"
 	"reflect"
 	"strings"
 
 	"github.com/exoscale/egoscale"
+	"github.com/go-ini/ini"
 	flag "github.com/spf13/pflag"
 )
 
@@ -17,6 +20,7 @@ func main() {
 	// XXX having all the methods! ~> pkgreflect
 	methods := []interface{}{
 		&egoscale.ListVirtualMachines{},
+		&egoscale.ListAPIs{},
 		&egoscale.ListZones{},
 		&egoscale.DeployVirtualMachine{},
 	}
@@ -56,6 +60,10 @@ func main() {
 	}
 
 	flagset := flag.NewFlagSet(command, flag.ExitOnError)
+	// global setting
+	var region string
+	flagset.StringVarP(&region, "region", "r", "cloudstack", "cloudstack.ini section name")
+
 	val := reflect.ValueOf(method)
 	// we've go a pointer
 	val = val.Elem()
@@ -64,9 +72,56 @@ func main() {
 	}
 	flagset.Parse(os.Args[2:])
 
-	out, _ := json.MarshalIndent(&method, "", "  ")
-	fmt.Print(command)
+	client, _ := buildClient(region)
+
+	// Request
+	//out, _ := json.MarshalIndent(&method, "", "  ")
+	//fmt.Println(string(out))
+	resp, err := client.Request(method.(egoscale.Command))
+	if err != nil {
+		out, _ := json.MarshalIndent(err, "", "  ")
+		log.Panicf(string(out))
+	}
+	out, _ := json.MarshalIndent(&resp, "", "  ")
 	fmt.Println(string(out))
+}
+
+func buildClient(region string) (*egoscale.Client, error) {
+	usr, _ := user.Current()
+	localConfig, _ := filepath.Abs("cloudstack.ini")
+	inis := []string{
+		localConfig,
+		filepath.Join(usr.HomeDir, ".cloudstack.ini"),
+	}
+	config := ""
+	for _, i := range inis {
+		if _, err := os.Stat(i); err != nil {
+			continue
+		}
+		config = i
+		break
+	}
+
+	if config == "" {
+		log.Panicf("Config file not found within: %s", strings.Join(inis, ", "))
+	}
+
+	cfg, err := ini.LoadSources(ini.LoadOptions{IgnoreInlineComment: true}, config)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	section, err := cfg.GetSection(region)
+	if err != nil {
+		log.Panicf("Section %q not found in the config file %s", region, config)
+	}
+	endpoint, _ := section.GetKey("endpoint")
+	key, _ := section.GetKey("key")
+	secret, _ := section.GetKey("secret")
+
+	client := egoscale.NewClient(endpoint.String(), key.String(), secret.String())
+
+	return client, nil
 }
 
 func populateVars(flagset *flag.FlagSet, value reflect.Value) error {
